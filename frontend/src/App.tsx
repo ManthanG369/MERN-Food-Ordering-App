208a46d89d75fcabebbd3247992235f7dcc4a594
Manthan Gaikwad <87421766+ManthanG369@users.noreply.github.com>
2024-03-07 13:07:56 +0530

    remove unused var

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-import { Button } from "./components/ui/button";
+// import { Button } from "./components/ui/button";
 
-function App() {
-  const [count, setCount] = useState(0);
+// function App() {
+//   const [count, setCount] = useState(0);
 
-  return <Button>Click me</Button>;
-}
+//   return <Button>Click me</Button>;
+// }
 
-export default App;
+// export default App;
