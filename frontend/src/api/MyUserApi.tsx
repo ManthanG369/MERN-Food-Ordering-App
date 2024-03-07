92cea9135811bae3da772dc8053a3ada55c8fdb7
Manthan Gaikwad <87421766+ManthanG369@users.noreply.github.com>
2024-03-07 12:28:01 +0530

    Add get user profile API and protected routes

--- frontend/src/api/MyUserApi.tsx
+++ frontend/src/api/MyUserApi.tsx
@@ -1,7 +1,45 @@
+import { User } from "@/types/User";
 import { useAuth0 } from "@auth0/auth0-react";
-import { useMutation } from "react-query";
+import { useMutation, useQuery } from "react-query";
+import { toast } from "sonner";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URI;
+
+export const userGetMyUser = () => {
+  const { getAccessTokenSilently } = useAuth0();
+
+  const getMyUserRequest = async (): Promise<User> => {
+    const accessToken = await getAccessTokenSilently();
+
+    const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch user");
+    }
+
+    return response.json();
+  };
+  const {
+    data: currentUser,
+    isLoading,
+    error,
+  } = useQuery("fetchCurrentUser", getMyUserRequest);
+
+  if (error) {
+    toast.error(error.toString());
+  }
+
+  return {
+    currentUser,
+    isLoading,
+  };
+};
 
 type CreateUserRequest = {
   auth0Id: string;
@@ -39,5 +77,54 @@ export const useCreateMyUser = () => {
     isLoading,
     isError,
     isSuccess,
+  };
+};
+
+type UpdateMyUserRequest = {
+  name: String;
+  addressLine1: String;
+  city: String;
+  country: String;
+};
+export const useUpdateMyUser = () => {
+  const { getAccessTokenSilently } = useAuth0();
+
+  const updateMyUserRequest = async (formData: UpdateMyUserRequest) => {
+    const accessToken = await getAccessTokenSilently();
+    const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to update user");
+    }
+
+    return response;
+  };
+
+  const {
+    mutateAsync: updateUser,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+    reset,
+  } = useMutation(updateMyUserRequest);
+
+  if (isSuccess) {
+    toast.success("User Profile updated");
+  }
+  if (error) {
+    toast.error(error.toString());
+    reset();
+  }
+  return {
+    updateUser,
+    isLoading,
   };
 };
--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -1,7 +1,24 @@
+import { useUpdateMyUser, userGetMyUser } from "@/api/MyUserApi";
 import UserProfileForm from "@/form/user-profile-form/UserProfileForm";
 
 const UserProfilePage = () => {
-  return <UserProfileForm />;
+  const { currentUser, isLoading: isGetLoading } = userGetMyUser();
+  const { updateUser, isLoading: isUpdateLoading } = useUpdateMyUser();
+
+  if (isGetLoading) {
+    return <span>Loading...</span>;
+  }
+
+  if (!currentUser) {
+    return <span>Unable to load user profile</span>;
+  }
+  return (
+    <UserProfileForm
+      currentUser={currentUser}
+      onSave={updateUser}
+      isLoading={isUpdateLoading}
+    />
+  );
 };
 
 export default UserProfilePage;
