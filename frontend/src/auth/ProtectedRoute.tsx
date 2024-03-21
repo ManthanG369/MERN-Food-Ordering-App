8e7fabbb99ee67f7a94c6e709152fadfc8b47a75
Manthan Gaikwad <87421766+ManthanG369@users.noreply.github.com>
2024-03-21 17:54:37 +0530

    Fix navigation issue

--- frontend/src/auth/ProtectedRoute.tsx
+++ frontend/src/auth/ProtectedRoute.tsx
@@ -1,5 +1,5 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import Loading from "@/components/ui/Loading";
 
 const ProtectedRoute = () => {
@@ -12,7 +12,8 @@ const ProtectedRoute = () => {
   if (isAuthenticated) {
     return <Outlet />;
   }
-  // return isAuthenticated ? <Outlet /> : <Navigate to="/" replace />;
+
+  return <Navigate to="/" replace />;
 };
 
 export default ProtectedRoute;
