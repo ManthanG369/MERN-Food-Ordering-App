7961b434c58d4cde95bb770449cbc6c6ba54133b
Manthan Gaikwad <87421766+ManthanG369@users.noreply.github.com>
2024-05-29 19:32:58 +0530

    add to cart and delete from cart added

--- frontend/src/pages/DetailPage.tsx
+++ frontend/src/pages/DetailPage.tsx
@@ -1,8 +1,11 @@
 import { useGetRestaurant } from "@/api/RestaurantApi";
 import MenuItem from "@/components/MenuItem";
+import OrderSummary from "@/components/OrderSummary";
 import RestaurantInfo from "@/components/RestaurantInfo";
 import Loading from "@/components/ui/Loading";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Card } from "@/components/ui/card";
+import { MenuItem as MenuItemType } from "@/types";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 
@@ -19,26 +22,82 @@ const DetailPage = () => {
   if (isLoading || !restaurant) {
     return <Loading />;
   }
+  const addToCart = (menuItem: MenuItemType) => {
+    setCartItems((prevCartItems) => {
+      // 1. check if the item is already in the
+      const existingCartItem = prevCartItems.find(
+        (cartItem) => cartItem._id === menuItem._id
+      );
 
+      let updatedCartItems;
+
+      // 2. if item is in cart , update the quantity
+      if (existingCartItem) {
+        updatedCartItems = prevCartItems.map((cartItem) =>
+          cartItem._id === menuItem._id
+            ? { ...cartItem, quantity: cartItem.quantity + 1 }
+            : cartItem
+        );
+      } else {
+        updatedCartItems = [
+          ...prevCartItems,
+          {
+            _id: menuItem._id,
+            name: menuItem.name,
+            price: menuItem.price,
+            quantity: 1,
+          },
+        ];
+      }
+
+      return updatedCartItems;
+    });
+  };
+  const removeFromCart = (cartItem: CartItem) => {
+    setCartItems((prevCartItems) => {
+      const updatedCartItems = prevCartItems.filter(
+        (item) => cartItem._id !== item._id
+      );
+
+      return updatedCartItems;
+    });
+  };
   return (
     <div className="flex flex-col gap-10">
       <AspectRatio ratio={16 / 5}>
         <img
-          className="rounded-md object-cover h-full w-full"
           src={restaurant.imageUrl}
-          alt=""
+          className="rounded-md object-cover h-full w-full"
         />
       </AspectRatio>
-
-      <div className="grid md:grid-cols-[4fr-2fr] gap-5 md:px-32">
+      <div className="grid md:grid-cols-[4fr_2fr] gap-5 md:px-32">
         <div className="flex flex-col gap-4">
           <RestaurantInfo restaurant={restaurant} />
-
-          <span className="text-2xl font-bold tracking-tight"></span>
-
+          <span className="text-2xl font-bold tracking-tight">Menu</span>
           {restaurant.menuItems.map((menuItem) => (
-            <MenuItem key={menuItem._id} menuItem={menuItem} />
+            <MenuItem
+              key={menuItem._id}
+              menuItem={menuItem}
+              addToCart={() => addToCart(menuItem)}
+            />
           ))}
+        </div>
+
+        <div>
+          <Card>
+            <OrderSummary
+              restaurant={restaurant}
+              cartItems={cartItems}
+              removeFromCart={removeFromCart}
+            />
+            {/* <CardFooter>
+              <CheckoutButton
+                disabled={cartItems.length === 0}
+                onCheckout={onCheckout}
+                isLoading={isCheckoutLoading}
+              />
+            </CardFooter> */}
+          </Card>
         </div>
       </div>
     </div>
